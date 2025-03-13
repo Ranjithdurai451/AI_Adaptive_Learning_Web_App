8fd618116bd9016747925b8976ee7cb3698e8173
Ranjithdurai451 <ranjithdurai111@gmail.com>
2025-03-13 06:53:48 +0000

    add redis for caching

--- backend/index.ts
+++ backend/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import { prerequisites } from './lib/data';
 import { generateQuiz, generateRoadmap } from './lib/utils';
 import cors from 'cors';
-
-
-
+import { createClient } from 'redis';
+import type { Response } from 'express';
+const redisClient = createClient({
+    url: process.env.REDIS_URL || ''
+});
 const app = express();
 app.use(cors({
     origin: "*",
@@ -14,59 +16,75 @@ app.use(express.json());
 
 
 
-app.get("/api/generate_quiz", async (req, res) => {
-    console.log("Triggered");
+app.get("/api/generate_quiz", async (req, res): Promise<any> => {
     try {
         const topic = req.query.topic as string;
         if (!topic) res.status(400).json({ error: "Topic is required" });
-
+        const cacheExists = await redisClient.get(topic);
+        if (cacheExists) {
+            console.log("Cache Hit!!");
+            return res.json(JSON.parse(cacheExists));
+        }
         const prerequisiteTopics = prerequisites[topic] || [];
 
         if (prerequisiteTopics.length > 0) {
             const prerequisiteQuiz = await generateQuiz(
                 prerequisiteTopics.join(", ")
             );
-            res.json(prerequisiteQuiz);
+            redisClient.set(topic, JSON.stringify(prerequisiteQuiz), {
+                EX: 86400
+            });
+            return res.json(prerequisiteQuiz);
         }
 
         const mainQuiz = await generateQuiz(topic);
-
-        res.json(mainQuiz);
+        redisClient.set(topic, JSON.stringify(mainQuiz), {
+            EX: 86400
+        });
+        return res.json(mainQuiz);
     } catch (error) {
         console.error("Error generating quiz:", error);
-        res.status(500).json({ error: "Error generating quiz" });
+        return res.status(500).json({ error: "Error generating quiz" });
     }
 });
 
 
 
-app.get("/api/generate_roadmap", async (req, res) => {
+app.get("/api/generate_roadmap", async (req, res): Promise<any> => {
     try {
         const topic = req.query.topic as string;
         const score = Number(req.query.score) || 0;
 
-        console.log("Topic:", topic);
-        console.log("Score:", score);
 
         if (!topic) {
-            res.status(400).json({ error: "Topic are required" });
+            return res.status(400).json({ error: "Topic are required" });
+        }
+        const cacheExists = await redisClient.get(`${topic}-${score}`);
+        if (cacheExists) {
+            console.log("Cache Hit!!");
+            return res.json(JSON.parse(cacheExists));
         }
 
 
         const roadmapData = await generateRoadmap(score, topic);
-        res.json(roadmapData);
+        redisClient.set(`${topic}-${score}`, JSON.stringify(roadmapData), {
+            EX: 86400
+        });
+        return res.json(roadmapData);
     } catch (error) {
         console.error("Error generating roadmap:", error);
-        res.status(500).json({ error: "Error generating roadmap" });
+        return res.status(500).json({ error: "Error generating roadmap" });
     }
 });
 
 
 
 const port = process.env.PORT || 3000;
-// const port = 3000;
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    redisClient.on("error", err => console.log(err));
+    await redisClient.connect();
+
     console.log(`Server running at http://localhost:${port}`);
 });
 
